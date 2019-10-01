044014947b11cb453648468de51aa0158c235f81
Kirill Sinitsyn <k.s.sinitsyn@gmail.com>
2019-10-01 23:55:39 +0300

    Init project

---
+++ routes/cards.js
@@ -0,0 +1,8 @@
+const cards = require('express').Router();
+const cardsData = require('../data/cards');
+
+cards.get('/', (req, res) => {
+  res.json(cardsData);
+});
+
+module.exports = cards
---
+++ routes/users.js
@@ -0,0 +1,17 @@
+const users = require('express').Router();
+const usersData = require('../data/users');
+
+users.get('/', (req,res) => {
+  res.json(usersData);
+});
+
+users.get('/:id', (req,res)=> {
+  const indexUser = usersData.findIndex((item) => item._id === req.params.id);
+  if (indexUser < 0) {
+    res.status(404).send({ message: 'Нет пользователя с таким id' });
+  } else {
+    res.send(usersData[indexUser]);
+  }
+})
+
+module.exports = users;
